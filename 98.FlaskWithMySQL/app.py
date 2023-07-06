f0cee7a1b7e0328619fbe2fbaf59b63387ec7e5b
Garam <festioal@naver.com>
2023-07-06 15:35:13 +0900

    0705

--- 98.FlaskWithMySQL/app.py
+++ 98.FlaskWithMySQL/app.py
@@ -1,51 +1,75 @@
-from flask import Flask , render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 import db.kpop_dao as kd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Flask with MySQL'
+    return render_template('00.hello.html')
 
 @app.route('/song/list')
 def song_list():
     songs = kd.get_song_list(20)
     return render_template('01.song_list.html', songs=songs)
 
-@app.route('/song/insert', methods=['GET', 'POST'])
-def song_insert():
+@app.route('/song/add', methods=['GET', 'POST'])
+def song_add():
     if request.method == 'GET':
-        return render_template('02.song_insert.html')
+        return render_template('02.song_add.html')
     else:
         title = request.form['title']
         lyrics = request.form['lyrics']
         kd.insert_song((title, lyrics))
         return redirect('/song/list')
-
+    
 @app.route('/song/update/<sid>', methods=['GET', 'POST'])
-def song_update():
+def song_update(sid):
     if request.method == 'GET':
-        return render_template('03.song_update.html')
+        song = kd.get_song(sid)
+        return render_template('03.song_update.html', song=song)
     else:
-        pass
+        title = request.form['title']
+        lyrics = request.form['lyrics']
+        kd.update_song((title, lyrics, sid))
+        return redirect('/song/list')
+    
+@app.route('/song/delete/<sid>')
+def song_delete(sid):
+    kd.delete_song(sid)
+    return redirect('/song/list')
 
 @app.route('/gg/list')
 def gg_list():
     groups = kd.get_girl_group_list(20)
     return render_template('11.gg_list.html', groups=groups)
 
-@app.route('/gg/insert', methods=['GET', 'POST'])
-def gg_insert():
+@app.route('/gg/add', methods=['GET', 'POST'])
+def gg_add():
     if request.method == 'GET':
-        return render_template('12.gg_insert.html')
+        return render_template('12.gg_add.html')
     else:
-        title = request.form['title']
-        lyrics = request.form['lyrics']
-        kd.insert_song((title, lyrics))
-        return redirect('/song/list')
+        name = request.form['name']
+        debut = request.form['debut']
+        hid = request.form['hid']
+        kd.insert_girl_group((name, debut, hid))
+        return redirect('/gg/list')
+    
+@app.route('/gg/update/<gid>', methods=['GET', 'POST'])
+def gg_update(gid):
+    if request.method == 'GET':
+        group = kd.get_girl_group(gid)
+        return render_template('13.gg_update.html', group=group)
+    else:
+        name = request.form['name']
+        debut = request.form['debut']
+        hid = request.form['hid']
+        kd.update_girl_group((name, debut, hid, gid))
+        return redirect('/gg/list')
+    
+@app.route('/gg/delete/<gid>')
+def gg_delete(gid):
+    kd.delete_girl_group(gid)
+    return redirect('/gg/list')
 
 if __name__ == '__main__': 
-    app.run(debug=True) # 디버그를 트루로 둬서 서버가 코드바꾸고 저장하면 자동으로 재시작함 100로는 아님
-
-
-
+    app.run(debug=True)     # 디버그를 트루로 둬서 서버가 코드바꾸고 저장하면 자동으로 재시작함 100로는 아님
